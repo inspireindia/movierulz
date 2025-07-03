d945879e0939fcc1ba7f89159be695c1850a8c3d
inspireindia <br.vijaykumarreddy@gmail.com>
2025-07-03 18:53:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -123,4 +123,6 @@ async def sse():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port)
